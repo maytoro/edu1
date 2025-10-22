c70862a8d9fc620cb52bcb3a1fafdfe55ee173c5
sanggilhan <ifsea@hanmail.net>
2025-10-23 08:30:11 +0900

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! Update Test v2"
+    return "Hello World! Update Test on 08:01, Oct 23 2025"
   
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port=8080,debug=True)
